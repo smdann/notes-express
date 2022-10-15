5ebb7a7b76d6eb0cf892fa8a048be4ba19d93543
SDann <smdann17@gmail.com>
2022-10-15 09:01:28 -0500

    Added routes to the main page and notes page using the app.get method. Using res.sendFile to send the user to the appropriate pages.

--- server.js
+++ server.js
@@ -10,8 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Routes
+// Route to homepage
+app.get("/", (req, res) => 
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
+// Route to notes page
+app.get("/notes", (req, res) => 
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 // Get notes - API 
 
