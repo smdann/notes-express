302b42a7a9ab6a7085067802fbbac09b5fb0b5b8
SDann <smdann17@gmail.com>
2022-10-17 07:05:26 -0500

    Created an API post for saving notes to the note database (db.json). New helper file created with a uuid function to assign each note with a unique id. Created a delete route that reads the db.json file and filters by target id in order to delete the correct note. Tested routes in Insomnia and routes are working but there's a minor bug with the deletion of notes. Will continue to debug.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const noteDatabase = require("./db/db.json");
+const uuid = require("./helpers/uuid");
 
 const PORT = 3001;
 const app = express();
@@ -20,14 +22,59 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// Get notes - API 
+// Get notes - API route
+app.get("/api/notes", (req, res) => 
+  res.json(noteDatabase)
+);
 
+// Save note - API route
+app.post("/api/notes", (req, res) => {
+  const fetchedNote = req.body
+  const {title, text} = fetchedNote
+  
+  // Saves the new note object to the newNote const
+  const newNote = {
+    title,
+    text,
+    id: uuid(),
+  };
+  console.log(newNote)
+  
+  // Pushes the new note to the global noteDatabase array
+  noteDatabase.push(newNote);
+  console.log(noteDatabase)
+  // Creates a stringified version to add to the db.json file
+  const stringedNotesDB = JSON.stringify(noteDatabase);
+  console.log(stringedNotesDB)
+  // Writes the stringified note to the file
+  fs.writeFile(path.join(__dirname, "/db/db.json"), stringedNotesDB, (err) => {
+    if(err){
+      res.status(400).json({msg: "There was an error saving your note."})
+    } else {
+      res.status(200).json({msg: "Your note was successfully saved."})
+    }
+  });
+  
+});
 
-// Save note - API
+// Delete note - api/notes/:id
+app.delete("/api/notes/:id", (req, res) => {
+  const rawData = fs.readFileSync(path.join(__dirname, "/db/db.json"));
+  const parsedData = JSON.parse(rawData)
+  console.log("@@@@@", parsedData)
+  const targetId = req.params.id;
+  console.log(targetId)
+  const filteredData = parsedData.filter(datum => datum.id !== targetId);
+  console.log(filteredData)
+  const stringedNotesDB = JSON.stringify(filteredData);
+  console.log(stringedNotesDB)
+  // Writes the stringified note to the file
+  fs.writeFileSync(path.join(__dirname, "/db/db.json"), stringedNotesDB);
+  const rawDataNew = fs.readFileSync(path.join(__dirname, "/db/db.json"));
+  const parsedDataNew = JSON.parse(rawDataNew)
+  res.json(parsedDataNew)
 
-
-// Delete note - API
-
+})
 
 
 
