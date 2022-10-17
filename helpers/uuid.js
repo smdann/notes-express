86afcc453dfd0d3fb550b94506f0d05f87887024
SDann <smdann17@gmail.com>
2022-10-17 17:46:34 -0500

    Updated const PORT to allow for hosting on Railway as well as local server 3001. Added a start script to package.json so that node server.js is started once application is hosted on Railway. Added comments to server.js and uuid.js files.

--- helpers/uuid.js
+++ helpers/uuid.js
@@ -1,3 +1,4 @@
+// Generates a string of random numbers and letters to be used for the note id
 module.exports = () =>
   Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const noteDatabase = require("./db/db.json");
 const uuid = require("./helpers/uuid");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
@@ -24,7 +24,6 @@ app.get("/notes", (req, res) =>
 
 // Get notes - API route
 app.get("/api/notes", (req, res) => 
-  // res.json(noteDatabase)
   {const rawDataNew = fs.readFileSync(path.join(__dirname, "/db/db.json"));
     const parsedDataNew = JSON.parse(rawDataNew)
     res.json(parsedDataNew)}
@@ -41,14 +40,13 @@ app.post("/api/notes", (req, res) => {
     text,
     id: uuid(),
   };
-  console.log(newNote)
   
   // Pushes the new note to the global noteDatabase array
   noteDatabase.push(newNote);
-  console.log(noteDatabase)
+  console.log(noteDatabase);
   // Creates a stringified version to add to the db.json file
   const stringedNotesDB = JSON.stringify(noteDatabase);
-  console.log(stringedNotesDB)
+  console.log(stringedNotesDB);
   // Writes the stringified note to the file
   fs.writeFile(path.join(__dirname, "/db/db.json"), stringedNotesDB, (err) => {
     if(err){
@@ -57,21 +55,27 @@ app.post("/api/notes", (req, res) => {
       res.status(200).json({msg: "Your note was successfully saved."})
     }
   });
-  
 });
 
-// Delete note - api/notes/:id
+// Delete note - API route
 app.delete("/api/notes/:id", (req, res) => {
+  // Reads and parses the db.json file data
   const rawData = fs.readFileSync(path.join(__dirname, "/db/db.json"));
   const parsedData = JSON.parse(rawData)
-  console.log("@@@@@", parsedData)
+  console.log(parsedData)
+  
+  // Id of note item clicked for deletion is set to targetId
   const targetId = req.params.id;
-  console.log(targetId)
+
+  // Filtered data excludes the datum with the target id
   const filteredData = parsedData.filter(datum => datum.id !== targetId);
   console.log(filteredData)
+  
+  // Stringifies the filtered data
   const stringedNotesDB = JSON.stringify(filteredData);
   console.log(stringedNotesDB)
-  // Writes the stringified note to the file
+  
+  // Writes the stringified notes to the db.json file
   fs.writeFileSync(path.join(__dirname, "/db/db.json"), stringedNotesDB);
   const rawDataNew = fs.readFileSync(path.join(__dirname, "/db/db.json"));
   const parsedDataNew = JSON.parse(rawDataNew)
@@ -79,9 +83,7 @@ app.delete("/api/notes/:id", (req, res) => {
 
 })
 
-
-
-
+// Express listener
 app.listen(PORT, () => 
   console.log(`App listening at http://localhost:${PORT}`)
 );
